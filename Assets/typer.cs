c7717ba94388fd15ac5479e7ef145fa0783f6c43
Fang <fang@dhcp-v036-106.mobile.uci.edu>
2018-01-27 13:32:02 -0800

    typer effect

---
+++ Assets/typer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class typer : MonoBehaviour {
+
+	Text txt;
+	string story;
+	public float speed;
+
+	void Awake () 
+	{
+		txt = GetComponent<Text> ();
+		story = txt.text;
+		txt.text = "";
+
+		// TODO: add optional delay when to start
+		StartCoroutine ("PlayText");
+	}
+
+	IEnumerator PlayText()
+	{
+		foreach (char c in story) 
+		{
+			txt.text += c;
+			yield return new WaitForSeconds (speed);
+		}
+	}
+
+}
