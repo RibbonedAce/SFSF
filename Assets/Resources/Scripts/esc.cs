4a841fe9769239642c7b43ae4cbf8b83f3ef62ee
Fang <fang@dhcp-v036-106.mobile.uci.edu>
2018-01-27 12:30:10 -0800

    basic UI

--- Assets/Resources/Scripts/esc.cs
+++ Assets/Resources/Scripts/esc.cs
@@ -7,12 +7,23 @@ using UnityEngine.UI;
 public class esc : MonoBehaviour {
 
 	public Button playButton;
+	public Button creditButton;
+	public Button endButton;
+	//public Button mainMenuButton;
 
 	// Use this for initialization
 	void Start () {
-		Button btn = playButton.GetComponent<Button>();
-		btn.onClick.AddListener(TaskOnClick);
+		Button playBtn = playButton.GetComponent<Button>();
+		playBtn.onClick.AddListener(playOnClick);
 		
+		Button creditBtn = creditButton.GetComponent<Button>();
+		creditBtn.onClick.AddListener(creditOnClick);
+
+		Button endBtn = endButton.GetComponent<Button>();
+		endBtn.onClick.AddListener(endOnClick);
+
+		// Button mainBtn = mainButton.GetComponent<Button>();
+		// mainBtn.onClick.AddListener(mainnClick);
 	}
 	
 	// Update is called once per frame
@@ -21,22 +32,26 @@ public class esc : MonoBehaviour {
 		{
 			SceneManager.LoadScene("escScene", LoadSceneMode.Single);
 		}
-
-		
 	}
 
-	void TaskOnClick()
+	void playOnClick()
 	{
-		if (playButton.tag == "play button")
-			SceneManager.LoadScene("escScene", LoadSceneMode.Single);
+		SceneManager.LoadScene("playScene", LoadSceneMode.Single);
 
-		if (playButton.tag == "main menu")
-			SceneManager.LoadScene("mainMenuScene", LoadSceneMode.Single);
-
-		if (playButton.tag == "credit")
-			SceneManager.LoadScene("creditScene", LoadSceneMode.Single);
-
-		if (playButton.tag == "end")
-			Application.Quit();
 	}
+
+	void creditOnClick()
+	{
+		SceneManager.LoadScene("creditScene", LoadSceneMode.Single);
+	}
+
+	void endOnClick()
+	{
+		Application.Quit();
+	}
+
+	// void mainOnClick()
+	// {
+	// 	SceneManager.LoadScene("mainScene", LoadSceneMode.Single);
+	// }
 }
