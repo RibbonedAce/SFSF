ffffc01cff2c0cf421e56b89768a8d7a6927df84
Tyler Gemora <tgemora@uci.edu>
2018-01-27 13:56:40 -0800

    Basic branching complete

---
+++ Assets/Resources/Scripts/PlayerScenario.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScenario {
+    public string startText;
+    public Choice choice;
+    public Dictionary<string, string> endText;
+
+    public PlayerScenario (string data)
+    {
+        string[] splitData = data.Split(new string[] { System.Environment.NewLine }, System.StringSplitOptions.None);
+        startText = splitData[0].Trim();
+        choice = new Choice(splitData[1].Trim());
+        endText = new Dictionary<string, string>();
+        string[] ends = splitData[2].Trim().Split('|');
+        endText.Add("AA", ends[0]);
+        endText.Add("AB", ends[1]);
+        endText.Add("BA", ends[2]);
+        endText.Add("BB", ends[3]);
+    }
+}
