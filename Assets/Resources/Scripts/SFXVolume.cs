05bc4912b5d0574bd87f6b1e30be07aee1620089
Tyler Gemora <tgemora@uci.edu>
2018-01-28 10:27:52 -0800

    Added back-end sound control

---
+++ Assets/Resources/Scripts/SFXVolume.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class SFXVolume : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update ()
+    {
+        GetComponent<AudioSource>().volume = VolumeController.sfxVolume;
+    }
+}
