03b66329fb8a5a69fcd4890397f45ba3b234e4ab
Howard Lam <howarl3@uci.edu>
2018-01-27 17:25:54 -0800

    Added blinking effect to typer script

--- Assets/Resources/Scripts/typer.cs
+++ Assets/Resources/Scripts/typer.cs
@@ -8,26 +8,54 @@ public class typer : MonoBehaviour {
 	Text txt;
 	string story;
 	public float speed;
-
+    public float blinkingSpeed;
 
 	void Awake () 
 	{
-		txt = GetComponent<Text> ();
+		txt = GetComponent<Text>();
 		story = txt.text;
 		txt.text = "";
 
+        transform.position = txt.transform.position;
 		// TODO: add optional delay when to start
 		StartCoroutine ("PlayText");
 	}
 
+    private void Update()
+    {
+        /*if (story == txt.text)
+        {
+            StartCoroutine(PlayBlinkingCursor());
+        }*/
+    }
 
-	IEnumerator PlayText()
+
+    IEnumerator PlayText()
 	{
 		foreach (char c in story) 
 		{
-			txt.text += c;
-			yield return new WaitForSeconds (speed);
-		}
-	}
+            txt.text += c;
+
+            string tempTxt = txt.text;
+            txt.text += " |";
+
+            //transform.position += Vector3.right;
+
+            yield return new WaitForSeconds(speed);
+            txt.text = tempTxt;
+        }
+        StartCoroutine(PlayBlinkingCursor());
+    }
+
+    IEnumerator PlayBlinkingCursor()
+    {
+        while (true)
+        {
+            txt.text += " |";
+            yield return new WaitForSeconds(blinkingSpeed);
+            txt.text = story;
+            yield return new WaitForSeconds(blinkingSpeed);
+        }
+    }
 
 }
